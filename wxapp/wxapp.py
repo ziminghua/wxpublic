56a25e3ae85ee4e3f9b5aedcf608402542673605
訾明华 <565209960@qq.com>
2016-12-19 19:20:45 +0800

    修正验证错误

--- wxapp/wxapp.py
+++ wxapp/wxapp.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+import hashlib
 from authentication.WXBizMsgCrypt import WXBizMsgCrypt
 
 appid = 'c41c084feb0d18dd1937ba989f667b42'
 token = 'ICcxs5844qOY24rTc1c696X5btR551m2'
 encodingAESKey = 'rEotmyYHNsLLXM1olf3ntRx2PXCYXK6eQ3CxWCJegSV'
 wxcpt = WXBizMsgCrypt(token, encodingAESKey, appid)
+
 
 class WxApp(object):
     @staticmethod
@@ -15,5 +17,12 @@ class WxApp(object):
         timestamp = request.values['timestamp']
         nonce = request.values['nonce']
         verifyEchoStr = request.values["echostr"]
-        ret, decryp_xml = wxcpt.VerifyURL(signature, timestamp, nonce, verifyEchoStr)
-        return decryp_xml
+        param_list = [token, timestamp, nonce]
+        param_list.sort()
+        sha1 = hashlib.sha1()
+        sha1.update(''.join(param_list).encode('utf-8'))
+        hashcode = sha1.hexdigest()
+        if hashcode == signature:
+            return verifyEchoStr
+        else:
+            return ''
