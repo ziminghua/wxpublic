0a3f3384f1420c25f476876bb7ac24fb7206e07e
訾明华 <565209960@qq.com>
2016-12-22 11:51:39 +0800

    添加日志用于调试

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from wxapp.wxapp import WxApp
 
 app = Flask(__name__)
 
+from werkzeug._internal import _log
+
 
 @app.route('/hello')
 def hello_world():
@@ -12,9 +14,11 @@ def hello_world():
 
 @app.route('/', methods=['GET', 'POST'])
 def wx_main():
+    _log("info", "收到请求")
     if "echostr" in request.values:
         return WxApp.is_valid(request)
     else:
+        _log("info", "被动回复")
         return WxApp.weather_search(request)
 
 
