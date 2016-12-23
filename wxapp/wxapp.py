7b4d2e7fcb77524fb8f4d01b12a9b1ab3de69a53
訾明华 <565209960@qq.com>
2016-12-23 12:16:45 +0800

    修改接口

--- wxapp/wxapp.py
+++ wxapp/wxapp.py
@@ -35,19 +35,17 @@ class WxApp(object):
     @staticmethod
     def weather_search(request):
         _log("info", "开始")
-        #signature, timestamp, nonce = WxApp.get_query_param(request)
-        #_log("info", "%s %s %s" % (signature, timestamp, nonce))
-        #content, from_user = WxApp.get_content(request)
-        #_log("info", "%s %s" % (content, from_user))
-        #ret, encrypt_xml = WxApp.send_data(content, from_user, nonce)
-        #_log("info", "%s %s" % (ret, encrypt_xml))
+        signature, timestamp, nonce = WxApp.get_query_param(request)
+        _log("info", "%s %s %s" % (signature, timestamp, nonce))
+        content, from_user = WxApp.get_content(request)
+        _log("info", "%s %s" % (content, from_user))
+        ret, encrypt_xml = WxApp.send_data(content, from_user, nonce)
+        _log("info", "%s %s" % (ret, encrypt_xml))
         #city_string = WxApp.get_content(request)
         #city_code = city_dic.city_dic[city_string]
         #url_open = urllib.urlopen("http://www.weather.com.cn/data/cityinfo/%s.html" % city_code)
         #s = url_open.read()
         #s = WxApp.get_content(request).encode("utf-8")
-
-        ret, encrypt_xml = WxApp.send_data("123", "oH83Zv1p39tc8e03CDVUtGsF-7ZI", "994978882")
 
         if ret == 0:
             return encrypt_xml
